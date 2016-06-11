269c03d7b373804f94d6f6cbd8c2d008c3c334f6
Dingcheng Yue <darwinsenior@gmail.com>
2016-06-11 15:34:51 -0700

    fix magit

--- magit.el
+++ magit.el
@@ -4,9 +4,9 @@
   "binding key in the magit mapping"
   (define-key magit-mode-map (kbd keybinding) action))
 
+(nmap "SPC m g" 'magit-status)
 (eval-after-load 'magit-mode-map
   '(progn
-    (nmap "SPC m g" 'magit-status)
     (mg-map "+" 'magit-stage)
     (mg-map "-" 'magit-unstage)
     (mg-map "[" 'magit-section-backward)
