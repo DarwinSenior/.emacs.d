1105591633b20981ab5d0a60bb5a98be46222aeb
Dingcheng Yue <darwinsenior@gmail.com>
2016-06-22 09:35:46 -0700

    using telephone line instead

--- appearance.el
+++ appearance.el
@@ -1,15 +1,29 @@
-(require-package 'powerline)
-(powerline-default-theme)
-(require-package 'airline-themes)
+;; create a new line
+;; (require-package 'powerline)
+;; (powerline-default-theme)
+;; (require-package 'airline-themes)
 
-; theme and all that stuff
+;; theme and all that stuff
 (require-package 'color-theme-sanityinc-tomorrow)
-(load-theme 'airline-molokai t)
 (load-theme 'sanityinc-tomorrow-night t)
 (set-default-font "Fantasque Sans Mono-21")
 (scroll-bar-mode -1)
 (menu-bar-mode -1)
 (tool-bar-mode -1)
+
+(require-package 'telephone-line)
+
+(setq telephone-line-lhs
+      '((evil . (telephone-line-evil-tag-segment))
+	(accent . (telephone-line-vc-segment
+		   telephone-line-erc-modified-channels-segment
+		   telephone-line-process-segment))
+	(nil . (telephone-line-buffer-segment))))
+(setq telephone-line-rhs
+      '((nil . (telephone-line-misc-info-segment))
+	(accent . (telephone-line-major-mode-segment))
+	(evil . (telephone-line-airline-position-segment))))
+(telephone-line-mode)
 
 (require-package 'nlinum-relative)
 (add-hook 'prog-mode-hook 'nlinum-relative-mode)
@@ -22,15 +36,14 @@
 (add-hook 'evil-operator-state-exit-hook
 	  (lambda () (when (bound-and-true-p nlinum-relative-mode) (nlinum-relative-off))))
 
-(require-package 'delight)
-(delight '((helm-mode nil helm)
-	   (yas-minor-mode nil yasnippet)
-	   (undo-tree-mode nil undo-tree)
-	   (golden-ratio-mode nil golden-ratio)
-	   (company-mode nil company)))
-
-;; (global-whitespace-newline-mode)
-;; (setq whitespace-style '(newline newline-mark))
-;; (setq whitespace-display-mappings
-;;       '((newline-mark 10 [8617 10])))
-;; (set-face-background 'whitespace-newline nil)
+;; (require-package 'delight)
+;; (delight '((auto-revert-mode nil auto-revert)
+;;	   (flycheck-mode nil flycheck)
+;;	   (evil-commentary-mode nil evil-commentary)
+;;	   (whitespace-cleanup-mode nil whitespace-cleanup)
+;;	   (smartparens-mode nil smartparens)
+;;	   (helm-mode nil helm)
+;;	   (yas-minor-mode nil yasnippet)
+;;	   (undo-tree-mode nil undo-tree)
+;;	   (golden-ratio-mode nil golden-ratio)
+;;	   (company-mode nil company)))
